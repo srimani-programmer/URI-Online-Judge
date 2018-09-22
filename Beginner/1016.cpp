f05c79fafb695282ff86008af819b3c9b4144e29
srimani-programmer <srimani.scholar@gmail.com>
2018-09-22 14:39:21 +0530

    Solution

---
+++ Beginner/1016.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int minutes;
+    cin>>minutes;
+
+    int totmin = minutes * 2;
+    cout<<totmin<<" minutos"<<endl;
+
+    return 0;
+}
