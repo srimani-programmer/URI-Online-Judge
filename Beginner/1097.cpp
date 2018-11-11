024cd41b6331976533eed4767879845eb12e515b
srimani-programmer <srimani.scholar@gmail.com>
2018-11-11 13:13:22 +0530

    Solution.

---
+++ Beginner/1097.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int i=1,j=7;
+
+    while(i<=9){
+        int count = 0;
+
+        while(count < 3){
+            cout<<"I="<<i<<" "<<"J="<<j<<endl;
+            j = j-1;
+            count++;
+        }
+        j = j+count+2;
+        i = i+2;
+    }
+
+    return 0;
+}
